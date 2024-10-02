f924f930c43a95d8695f93b5a9fa17a53e02f264
Crystal <99391710+x200706@users.noreply.github.com>
2024-10-02 23:13:22 +0800

    更新欄位label

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with st.sidebar:
 if menu == "甘特圖產生器":
     try:
         st.subheader("甘特圖產生器")
-        data_from_user = st.text_area("輸入事件與區間", placeholder = """201509,202107,大學+研究所（文史哲領域）
+        data_from_user = st.text_area("輸入區間及對應事件", placeholder = """201509,202107,大學+研究所（文史哲領域）
 202107,202109,協助學校教授工作""")
         colors_from_user = st.text_input("甘特圖配色", value = "#917FB3,#E5BEEC,#FDE2F3", placeholder = "#917FB3,#E5BEEC,#FDE2F3")
         if st.button("生成甘特圖"):
